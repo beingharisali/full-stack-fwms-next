fd664b3c32071b19a047cecce82484c57193ec10
MinahilTech <minahilmahi2006@gmail.com>
2025-12-24 12:53:50 +0500

    role

---
+++ app/manager/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../component/navbar'
+import Sidebar from '../component/sidebar'
+
+function Managerpage() {
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Managerpage
