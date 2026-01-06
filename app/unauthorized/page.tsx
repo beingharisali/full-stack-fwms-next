5bcbe1d9da780cbb5274141d76ccb9d069447f33
CodeJaveria <javeriasharif145@gmail.com>
2026-01-06 16:57:40 +0500

    Task done

---
+++ app/unauthorized/page.tsx
@@ -0,0 +1,7 @@
+export default function Unauthorized() {
+  return (
+    <div className="h-screen flex items-center justify-center text-xl font-semibold">
+      ❌ You are not allowed to access this page
+    </div>
+  );
+}
